c73e4e8ff80da99b3cb06b495d3c38ba268027cc
hyuk <k941026h@naver.com>
2024-01-25 23:10:21 +0900

    application.yml dev phase jpa ddl auto option 변경 / FilterConfig Cors 설정 변경

--- src/main/java/com/ndm/core/config/FilterConfig.java
+++ src/main/java/com/ndm/core/config/FilterConfig.java
@@ -1,9 +1,11 @@
 package com.ndm.core.config;
 
 import com.ndm.core.filter.AttributeFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -11,20 +13,23 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class FilterConfig {
 
+    @Value("${client.location}")
+    private String clientLocation;
+
     @Bean
     public FilterRegistrationBean<CorsFilter> corsFilterFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(false);
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin(clientLocation);
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
-        config.setMaxAge(6000L);
+        config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
 
         FilterRegistrationBean<CorsFilter> filterRegistrationBean = new FilterRegistrationBean<>(new CorsFilter(source));
-        filterRegistrationBean.setOrder(Integer.MIN_VALUE);
+        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 
         return filterRegistrationBean;
     }
